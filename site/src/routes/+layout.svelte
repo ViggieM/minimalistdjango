6954372f2355afd82eb37534cf22773dcaa236fc
Victor <victormovileanu@gmail.com>
2025-03-01 15:38:45 +0100

    Run prettier

--- site/src/routes/+layout.svelte
+++ site/src/routes/+layout.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
-  import '../app.css';
+	import '../app.css';
 
-  let { children } = $props();
+	let { children } = $props();
 </script>
 
 <nav class="flex justify-center">
-  <ul class="flex gap-4 bg-black pt-2 pb-3 px-5 rounded-b-xl">
-    <li class="uppercase"><a href="/" class="nav-item">Home</a></li>
-    <li class="uppercase"><a href="" class="nav-item">TIL</a></li>
-    <li class="uppercase"><a href="" class="nav-item">Articles</a></li>
-    <li class="uppercase"><a href="" class="nav-item">Tools</a></li>
-    <li class="uppercase"><a href="" class="nav-item">Snippets</a></li>
-  </ul>
+	<ul class="flex gap-4 rounded-b-xl bg-black px-5 pt-2 pb-3">
+		<li class="uppercase"><a href="/" class="nav-item">Home</a></li>
+		<li class="uppercase"><a href="" class="nav-item">TIL</a></li>
+		<li class="uppercase"><a href="" class="nav-item">Articles</a></li>
+		<li class="uppercase"><a href="" class="nav-item">Tools</a></li>
+		<li class="uppercase"><a href="" class="nav-item">Snippets</a></li>
+	</ul>
 </nav>
 
 {@render children()}
