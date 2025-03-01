9b37af0bb9eb574c55d468913aa42619ae0044c4
Victor <victormovileanu@gmail.com>
2025-03-01 14:59:25 +0100

    Add navbar

--- site/src/routes/+layout.svelte
+++ site/src/routes/+layout.svelte
@@ -1,6 +1,17 @@
 <script lang="ts">
-	import '../app.css';
-	let { children } = $props();
+  import '../app.css';
+
+  let {children} = $props();
 </script>
+
+<nav class="flex justify-center">
+  <ul class="flex gap-4 bg-black pt-2 pb-3 px-5 rounded-b-xl">
+    <li class="uppercase"><a href="/" class="nav-item">Home</a></li>
+    <li class="uppercase"><a href="" class="nav-item">TIL</a></li>
+    <li class="uppercase"><a href="" class="nav-item">Articles</a></li>
+    <li class="uppercase"><a href="" class="nav-item">Tools</a></li>
+    <li class="uppercase"><a href="" class="nav-item">Snippets</a></li>
+  </ul>
+</nav>
 
 {@render children()}
