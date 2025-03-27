d118cc6d9a2de88714fcab7bf42f435d557121f4
Victor <victormovileanu@gmail.com>
2025-03-27 19:47:11 +0100

    transform hrefs with .md ending to regular links in HTML output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,27 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { visit } from 'unist-util-visit';
 
-
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://minimalistdjango.com/",
+  site: 'https://minimalistdjango.com/',
 
   vite: {
     plugins: [tailwindcss()]
+  },
+
+  markdown: {
+    rehypePlugins: [
+      () => (tree) => {
+        visit(tree, 'element', function(node, index, parent) {
+          if (node.tagName === 'a' && String(node.properties.href).endsWith('.md')) {
+            node.properties.href = String(node.properties.href).replace(/\.md$/, "")
+          }
+        });
+      }
+    ]
   }
 });
